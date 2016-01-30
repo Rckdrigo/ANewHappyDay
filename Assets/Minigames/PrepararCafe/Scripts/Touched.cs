13f0dbc5628aacbc1528178b9d2e18efeef37f50
LilShadowEX <shadow.wii@hotmail.com>
2016-01-30 08:48:38 -0600

    Agregue PrepararCafe

---
+++ Assets/Minigames/PrepararCafe/Scripts/Touched.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using StyloGestures;
+using UnityEngine.UI;
+
+public class Touched : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		}
+		
+
+
+	void Update()
+	{
+
+	}
+	void OnMouseDown()
+	{
+		Debug.Log ("Esta Tocandome D:!!!" + name);
+	}
+	// Update is called once per frame
+
+ }
+
