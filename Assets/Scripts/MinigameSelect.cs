81e4a1f61ccf91b791648ad31a1d69458b399626
Rckdrigo <rckdigomed@gmail.com>
2016-01-30 15:36:02 -0600

    Reordering

---
+++ Assets/Scripts/MinigameSelect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MinigameSelect : MonoBehaviour
+{
+
+    public string scene;
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
