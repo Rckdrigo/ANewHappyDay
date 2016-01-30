ee29d2f86d4152644ccd6bfda61735cd6a77fbb0
Rckdrigo <rckdigomed@gmail.com>
2016-01-30 04:47:49 -0600

    TeethBrusher

---
+++ Assets/TeethBrusher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TeethBrusher : MonoBehaviour
+{
+	public List<GameObject> teeth;
+	public static TeethBrusher Instance;
+
+	void Start ()
+	{
+		Instance = this;
+	}
+
+	public void RemoveStain (GameObject stain)
+	{
+		teeth.Remove (stain);
+		if (teeth.Count == 0)
+			print ("GANASTE");
+	}
+}
