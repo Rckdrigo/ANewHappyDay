dfc32b84a5df5024d537e187865a9e20823b2b5a
Rckdrigo <rckdigomed@gmail.com>
2016-01-30 15:36:48 -0600

    Fixing merge issues

---
+++ Assets/Minigames/Water/Scripts/SplashStream.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashStream : MonoBehaviour {
+
+    public GameObject splashGO;
+    private GameObject splash;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Show()
+    {
+        Debug.Log("TODO SPLASH");
+    }
+
+    void Hide()
+    {
+        Debug.Log("TODO HIDE");
+    }
+}
